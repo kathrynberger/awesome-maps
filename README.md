717ba0df4788d622c0c07ae16245f74ba0c4ae11
kathrynberger <kathryn@developmentseed.org>
2022-04-01 16:12:35 +0100

    added interactive filtering of dataframe

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # awesome-maps
+
+to run streamlit app from terminal:
+python -m streamlit run edinburgh_city_map.py
--- edinburgh_city_map.py
+++ edinburgh_city_map.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import pydeck as pdk
+import numpy as np
 import streamlit as st
 
 st.title("Where to Go When You've Got to Go in the City of Edinburgh")
@@ -16,18 +18,24 @@ geom = pd.DataFrame(data.tolist(), columns=['lat', 'lon'], index=df.index) \
 df = df.join(geom)
 df.fillna('', inplace=True)
 df.drop(["Refurbished in 2013", "Refurbishment status", "Email", "Telephone", "Location"], axis=1, inplace=True)
+
+# Data cleaning
+
+
+
+st.sidebar.markdown('### Data Filters')
+
+charge = list(df['Charge'].drop_duplicates())
+charge_choice = st.sidebar.multiselect('Select if there is a cost:', charge)
+facilities = list(df["Facilities"].drop_duplicates())
+facilities_choice = st.sidebar.multiselect('Select by available facilities:', facilities)
+# hours = list(df["Opening times"].drop_duplicates())
+# hours_choice = st.sidebar.multiselect('Select by opening times:', hours)
+
+df = df[df['Charge'].isin(charge_choice)]
+df = df[df['Facilities'].isin(facilities_choice)]
+# df = df[df['Opening times'].isin(hours_choice)]
+
 st.write(df)
-
-charge = df['Charge'].drop_duplicates()
-charge_choice = st.sidebar.selectbox('Select if there is a cost:', charge)
-facilities = df["Facilities"]
-facilities_choice = st.sidebar.selectbox('Select by available facilities:', facilities)
-hours = df["Opening times"]
-hours_choice = st.sidebar.selectbox('Select by opening times:', hours)
-
-
-
-
-
 
 st.map(df)
