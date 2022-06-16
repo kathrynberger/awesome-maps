933a427aa5b40964ca8b18e689a95a608c8453f6
kathrynberger <kathryn@developmentseed.org>
2022-06-16 15:47:19 +0100

    Revisions to readme, comments and table layout

--- README.md
+++ README.md
@@ -1,14 +1,20 @@
-# Simple geospatial demo app
 
+# A simple geospatial demo app
+This demo is meant to be a fun way to explore geospatial data and see how easy it is to develop a simple interactive web app with very few lines of code. Explore the commented code base to find out how it was done!
+
+# launching the app
 to run streamlit app from terminal:
+
 python -m streamlit run edinburgh_city_map.py
 
+Access to the app itself can be found on Streamlit Cloud here: 
+https://share.streamlit.io/kathrynberger/awesome-maps/main/edinburgh_city_map.py
 
 # requirements
 written in: 
 python 3.9
 
-requires the following to be imported:
+requires the following:
 pandas==1.4.2
 Pillow==9.1.1
 pydeck==0.7.1
--- edinburgh_city_map.py
+++ edinburgh_city_map.py
@@ -20,29 +20,32 @@ with col3:
 
 # use st.write() to easily enter text and hyperlinks
 st.write("Use this app to identify the nearest washroom. Keep in mind, however, this data was last updated in 2015...so you may be out of luck!")
-st.write("This demo is meant to show how easy it is to develop an interactive geospatial web application with only a few lines of code. Explore the code base to find out how. [Data source](https://github.com/edinburghcouncil/datasets/blob/master/Public%20toilets.csv)")
+st.write("This demo is meant to show how easy it is to develop an interactive geospatial web application with only a few lines of code. Explore the code base to find out how.")
 
 # Commented lines of code will not be read or visible
 # data source: https://github.com/edinburghcouncil/datasets/blob/master/Public%20toilets.csv
-# last updated in 2015
+# data source last updated in 2015
 
 # Reading in the dataset and some pre-processing to drop unused columns
 df = pd.read_csv("edinburgh_public_toilets.csv")
+# splitting location into lat, long
 data = df['Location'].str.strip('()').str.split(',').str[:2]
+# adding geometry
 geom = pd.DataFrame(data.tolist(), columns=['lat', 'lon'], index=df.index) \
          .apply(pd.to_numeric, errors='coerce')
 df = df.join(geom)
+# if data entry is empty (NA), replace with 'No additional services'
 df.fillna('No additional services', inplace=True)
+# drop all unnessary other columns
 df.drop(["Refurbished in 2013", "Refurbishment status", "Email", "Telephone", "Location"], axis=1, inplace=True)
 
 # table with the full dataset
-st.write("Scroll through the full table below for the full list and use the filters on the left-hand panel to filter based on your needs.")
+st.write("Scroll through the table below for the full list and use the filters on the left-hand panel to filter based on your needs.")
 st.write(df)
 
 # use Streamlit's side bar options to allow for filtering of dataset by pulldown options
 st.sidebar.markdown('### Data Filters')
 charge = list(df['Charge'].drop_duplicates())
-
 # multiselect allows you to select more than one filter option at a time
 charge_choice = st.sidebar.multiselect('Select if there is a cost:', charge)
 facilities = list(df["Facilities"].drop_duplicates())
@@ -51,10 +54,13 @@ facilities_choice = st.sidebar.multiselect('Select by available facilities:', fa
 df = df[df['Charge'].isin(charge_choice)]
 df = df[df['Facilities'].isin(facilities_choice)]
 
+# create spacing in the layout
 st.write('')
+# adding more descriptive text
 st.write("After filtering, find your results below. Use the interactive map below to zoom in further and hover over the point(s) on the map to find out more information.")
 st.write('')
-st.write(df)
+# produce a table of the filtered dataframe 
+st.table(df) 
 
 # defining tool tips to allow for pop up labels on map 
 # these labels are linked to columns in the original dataframe
@@ -66,6 +72,7 @@ tooltip={
    }
 }
 
+# Use Streamlit's pydeck_chart to define the layers shown in the map (see docs: https://docs.streamlit.io/library/api-reference/charts/st.pydeck_chart)
 r =   st.pydeck_chart(pdk.Deck(
         map_style='mapbox://styles/mapbox/light-v9',
         tooltip = tooltip,
